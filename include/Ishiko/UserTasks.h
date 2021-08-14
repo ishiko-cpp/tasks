110d7a957ed35179e71bbcf2bf28efd715853bd5
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-08-14 20:16:27 +0100

    Updates

---
+++ include/Ishiko/UserTasks.h
@@ -0,0 +1,17 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/user-tasks/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_USERTASKS_H_
+#define _ISHIKO_CPP_USERTASKS_H_
+
+#include "UserTasks/SyncFunctionTask.h"
+#include "UserTasks/Task.h"
+#include "UserTasks/TaskRunner.h"
+#include "UserTasks/Tasks.h"
+
+#include "UserTasks/linkoptions.h"
+
+#endif
