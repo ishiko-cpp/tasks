1da24b684e5589a99a325817ed63b1bc6b878d0e
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-12-18 14:41:24 +0000

    Rename header file (#42)

---
+++ include/Ishiko/Workflows.hpp
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2021-2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/workflows/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_WORKFLOWS_HPP_
+#define _ISHIKO_CPP_WORKFLOWS_HPP_
+
+#include "Workflows/linkoptions.hpp"
+#include "Workflows/SyncFunctionTask.hpp"
+#include "Workflows/TaskRunner.hpp"
+#include "Workflows/Tasks.hpp"
+#include "Workflows/UserTask.hpp"
+
+#endif
